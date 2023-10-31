93bf76166a316771a8fa760a4ebfb67310b0e4e5
Jonathan Schnitzler <59749440+Joni32do@users.noreply.github.com>
2023-10-31 17:08:24 +0000

    [Discretization .h] header files from doxygen

--- src/discretization/discretization.h
+++ src/discretization/discretization.h
@@ -1,13 +1,27 @@
 #pragma once 
+#include <array>
+#include <vector>
+#include "staggered_grid.h"
 
-class Discretization
+class Discretization : public StaggeredGrid
 {
     public:
-        Discretization();
-        auto nCells();
-        auto u();
-        auto v();
-        auto p();
-        auto f();   //not pretty but implemented like this in the output_writer
-        auto g();   //not pretty but implemented like this in the output_writer
+        Discretization(std::array<int, 2> nCells, std::array<double, 2> meshWidth);
+        // 1st derivatives
+        virtual double computeDu2Dx(int i, int j) const = 0;
+        virtual double computeDv2Dy(int i, int j) const = 0;
+
+        // mixed derivatives for advection
+        virtual double computeDuvDx(int i, int j) const = 0;
+        virtual double computeDuvDy(int i, int j) const = 0;
+
+        // 2nd derivatives for diffusion
+        virtual double computeD2uDx2(int i, int j) const = 0;
+        virtual double computeD2uDy2(int i, int j) const = 0;
+        virtual double computeD2vDx2(int i, int j) const = 0;
+        virtual double computeD2uDy2(int i, int j) const = 0;
+        
+        // compute pressure derivatives
+        virtual double computeDpDx(int i, int j) const = 0;
+        virtual double computeDpDy(int i, int j) const = 0;
 };
---
+++ src/discretization/field_variable.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <array>
+#include <vector>
+#include "../array2D/array2D.h"
+
+/**
+ * @class FieldVariable
+ * @brief 
+ * 
+ * A field variable is the discretization of a scalar function f(x) with x in the computational domain. 
+ * More specifically, a scalar value is stored at discrete nodes/points. 
+ * The nodes are arranged in an equidistant mesh with specified mesh width.
+*/
+class FieldVariable: public Array2D
+{
+    public:
+        FieldVariable(std::array<int,2> size, std::array<double,2> origin, std::array<double,2> meshWidth);
+        /**
+         * @brief get the value at the Cartesian coordinate (x,y). The value is linearly interpolated between stored points. 
+        */ 
+        double interpolateAt(double x, double y) const;
+    
+    private:
+        const std::array<double,2> origin_;
+        const std::array<double,2> meshWidth_;
+
+};
