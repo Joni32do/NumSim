3eed3ad0462205725275ea799e2e8153c630983d
Jonathan Schnitzler <jonathanschnitzler@web.de>
2023-11-05 00:57:38 +0100

    [Test][CD/DC] some more working tests

--- tests/test_central_differences.cpp
+++ tests/test_central_differences.cpp
@@ -1,12 +1,36 @@
 #include <gtest/gtest.h>
 #include "../src/discretization/central_differences.h"
 
+
+TEST(CentralDifferences, ComputeDpDx){
+    std::array<int,2> n_cells = {4,3};
+    std::array<double,2> meshWidth = {1.0, 1.0};
+    CentralDifferences cd_1(n_cells, meshWidth);
+    cd_1.p(1,1) = 1;
+    cd_1.p(2,1) = -1;
+
+    EXPECT_EQ(cd_1.computeDpDx(1, 1), -2.0);
+};
+
+
+TEST(CentralDifferences, ComputeDpDy){
+    std::array<int,2> n_cells = {4,3};
+    std::array<double,2> meshWidth = {1.0, 1.0};
+    CentralDifferences cd_1(n_cells, meshWidth);
+    cd_1.p(1,1) = 1;
+    cd_1.p(1,2) = -1;
+
+    EXPECT_EQ(cd_1.computeDpDy(1, 1), -2.0);
+};
+
+
 TEST(CentralDifferences, ComputeDu2Dx){
     std::array<int,2> n_cells = {4,3};
     std::array<double,2> meshWidth = {1.0, 1.0};
     CentralDifferences cd_1(n_cells, meshWidth);
-    cd_1.u(1,0) = 1;
     cd_1.u(0,1) = -1;
+    cd_1.u(1,1) = 0;
+    cd_1.u(2,1) = 1;
 
     EXPECT_EQ(cd_1.computeDu2Dx(1, 1), 0.0);
 };
--- tests/test_donor_cell.cpp
+++ tests/test_donor_cell.cpp
@@ -6,8 +6,9 @@ TEST(DonorCell, ComputeDu2Dx){
     std::array<double,2> meshWidth = {1.0, 1.0};
     double alpha = 0.5;
     DonorCell dc_1(n_cells, meshWidth, alpha);
-    dc_1.u(1,0) = 1;
     dc_1.u(0,1) = -1;
+    dc_1.u(1,1) = 0;
+    dc_1.u(2,1) = 1;
 
     EXPECT_EQ(dc_1.computeDu2Dx(1, 1), 0.0);
 };
