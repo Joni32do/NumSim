48241ff5d8bfd2f7cdb05fe7b78879f9da106e55
Jonathan Schnitzler <jonathanschnitzler@web.de>
2023-11-05 00:43:36 +0100

    [Test][Debug] in MakeFile ../src/discretization/disc... was missing

--- src/discretization/central_differences.h
+++ src/discretization/central_differences.h
@@ -10,8 +10,8 @@ public:
     CentralDifferences(std::array<int, 2> nCells, std::array<double, 2> meshWidth);
 
     // Additional derivatives for central differences scheme
-    virtual double computeDu2Dx(int i, int j) const override;
-    virtual double computeDv2Dy(int i, int j) const override;
-    virtual double computeDuvDx(int i, int j) const override;
-    virtual double computeDuvDy(int i, int j) const override;
+    double computeDu2Dx(int i, int j) const override;
+    double computeDv2Dy(int i, int j) const override;
+    double computeDuvDx(int i, int j) const override;
+    double computeDuvDy(int i, int j) const override;
 };
--- src/discretization/donor_cell.h
+++ src/discretization/donor_cell.h
@@ -10,10 +10,10 @@ public:
     DonorCell(std::array<int, 2> nCells, std::array<double, 2> meshWidth, double alpha);
 
     // Additional derivatives for donor cell scheme
-    virtual double computeDu2Dx(int i, int j) const override;
-    virtual double computeDv2Dy(int i, int j) const override;
-    virtual double computeDuvDx(int i, int j) const override;
-    virtual double computeDuvDy(int i, int j) const override;
+    double computeDu2Dx(int i, int j) const override;
+    double computeDv2Dy(int i, int j) const override;
+    double computeDuvDx(int i, int j) const override;
+    double computeDuvDy(int i, int j) const override;
 
 private:
     const double alpha_;
