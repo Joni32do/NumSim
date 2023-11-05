9619cbd9e13f0c1f06256622ff423634ca9639cd
Jonathan Schnitzler <jonathanschnitzler@web.de>
2023-11-05 11:07:20 +0100

    [Test][Central_Differences] TODO: DonorCell Tests with different alpha

--- tests/discrectization_tests.md
+++ tests/discrectization_tests.md
@@ -25,6 +25,11 @@
 
 
 ## DonorCell
+As a grid we use
+![grid](media/docGrid.svg)
 
+TODO:
+Here I want to draw in the calculations I do in the test files (but Inkscape is striking in the moment)
+![grid_with_annotation](media/docGrid_convection.svg)
 
 ## CentralDifferences
--- tests/test_central_differences.cpp
+++ tests/test_central_differences.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "../src/discretization/central_differences.h"
 
+// Pressure terms
 
 TEST(CentralDifferences, ComputeDpDx){
     std::array<int,2> n_cells = {4,3};
@@ -24,9 +25,54 @@ TEST(CentralDifferences, ComputeDpDy){
     EXPECT_EQ(cd_1.computeDpDy(1, 1), -2.0);
 };
 
+// Diffusion terms
+
+TEST(CentralDifferences, ComputeD2uDx2){
+    std::array<int,2> n_cells = {4,3};
+    std::array<double,2> meshWidth = {1.0, 1.0};
+    CentralDifferences cd_1(n_cells, meshWidth);
+    cd_1.u(1,2) = 1;
+    cd_1.u(2,2) = -1;
+    cd_1.u(3,2) = 1;
+
+    EXPECT_EQ(cd_1.computeD2uDx2(2, 2), 4.0);
+};
 
 
+TEST(CentralDifferences, ComputeD2uDy2){
+    std::array<int,2> n_cells = {4,3};
+    std::array<double,2> meshWidth = {1.0, 1.0};
+    CentralDifferences cd_1(n_cells, meshWidth);
+    cd_1.u(2,1) = 1;
+    cd_1.u(2,2) = -1;
+    cd_1.u(2,3) = 1;
 
+    EXPECT_EQ(cd_1.computeD2uDy2(2, 2), 4.0);
+};
+
+
+TEST(CentralDifferences, ComputeD2vDx2){
+    std::array<int,2> n_cells = {4,3};
+    std::array<double,2> meshWidth = {1.0, 1.0};
+    CentralDifferences cd_1(n_cells, meshWidth);
+    cd_1.v(1,2) = 1;
+    cd_1.v(2,2) = -1;
+    cd_1.v(3,2) = 1;
+
+    EXPECT_EQ(cd_1.computeD2vDx2(2, 2), 4.0);
+};
+
+
+TEST(CentralDifferences, ComputeD2vDy2){
+    std::array<int,2> n_cells = {4,3};
+    std::array<double,2> meshWidth = {1.0, 1.0};
+    CentralDifferences cd_1(n_cells, meshWidth);
+    cd_1.v(2,1) = 1;
+    cd_1.v(2,2) = -1;
+    cd_1.v(2,3) = 1;
+
+    EXPECT_EQ(cd_1.computeD2vDy2(2, 2), 4.0);
+};
 
 // Convection terms
 
@@ -75,7 +121,7 @@ TEST(CentralDifferences, ComputeDuvDy){
     double right = 0.5;
     double expected = (left - right) / (meshWidth[1]);
 
-    EXPECT_EQ(cd_1.computeDuvDy(2, 2), -1.0);
+    EXPECT_EQ(cd_1.computeDuvDy(2, 2), expected);
 };
 
 
@@ -85,21 +131,22 @@ TEST(CentralDifferences, ComputeDuvDx){
     CentralDifferences cd_1(n_cells, meshWidth);
     cd_1.v(2,2) = 0;
     
-    cd_1.u(1,2) = -1;
-    cd_1.u(1,3) = -1;
-    // -> u(1, 2.5) = -1
-    cd_1.v(1,2) = 1;
-    // -> v(1.5,2) = 0.5
-    double left = -0.5;
-
     cd_1.u(2,2) = -1;
     cd_1.u(2,3) = -1;
     // -> u(2, 2.5) = -1
     cd_1.v(3,2) = -1;
     // -> u(2.5,2) = -0.5
-    double right = 0.5;
-    double expected = (left - right) / (meshWidth[1]);
+    double left = 0.5;
 
-    EXPECT_EQ(cd_1.computeDuvDy(2, 2), -1.0);
+    cd_1.u(1,2) = -1;
+    cd_1.u(1,3) = -1;
+    // -> u(1, 2.5) = -1
+    cd_1.v(1,2) = 1;
+    // -> v(1.5,2) = 0.5
+    double right = -0.5;
+
+    double expected = (left - right) / (meshWidth[0]);
+
+    EXPECT_EQ(cd_1.computeDuvDx(2, 2), expected);
 };
 
