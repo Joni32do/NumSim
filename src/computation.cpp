28794aa0c71ca6b12a294bc4a07c0d6edbc54ad1
Jonathan Schnitzler <jonathanschnitzler@web.de>
2023-11-06 17:09:19 +0100

    [RHS][F][G]

--- src/computation.cpp
+++ src/computation.cpp
@@ -15,7 +15,9 @@ void Computation::initialize(int argc, char *argv[]){
   settings_.loadFromFile(filename);
 
   // TODO: Logger with settings
+
   //   settings.printSettings();
+
   std::array<double, 2> meshWidth_ = {settings_.physicalSize[0] / settings_.nCells[0],
                                       settings_.physicalSize[1] / settings_.nCells[1]};
   
@@ -26,7 +28,7 @@ void Computation::initialize(int argc, char *argv[]){
   outputWriterParaview_ = std::make_unique<OutputWriterParaview>(discretization_);
   outputWriterText_ = std::make_unique<OutputWriterText>(discretization_);
   
-  
+  // TODO:
   dt_ = 0.1;
 }
 
@@ -91,9 +93,95 @@ void Computation::computeTimeStepWidth()
 
 void Computation::computePreliminaryVelocities()
 {
-    // Set the bondary values for F and G
+    // Compute F
+    int i_beg = discretization_->uIBegin();
+    int i_end = discretization_->uIEnd();
+    int j_beg = discretization_->uJBegin();
+    int j_end = discretization_->uJEnd();
 
 
+    // Vertical
+    for(int j = j_beg + 1; j <= j_end - 1; j++){
+        discretization_->f(i_beg, j) = discretization_->u(i_beg, j);
+        discretization_->f(i_end, j) = discretization_->u(i_end, j);
+    }
+
+    // Horizontal
+    for(int i = i_beg + 1; i <= i_end - 1; i++){
+        discretization_->f(i, j_beg) = discretization_->u(i, j_beg);
+        discretization_->f(i, j_end) = discretization_->u(i, j_end);
+    }
+
+
+    // CInterior
+    for(int i = i_beg + 1; i <= i_end - 1; i++){
+        for(int j = j_beg + 1 ; j <= j_end - 1; j++){
+
+            double diffusion = 1/settings_.re * (discretization_->computeD2uDx2(i, j) 
+                                               + discretization_->computeD2uDy2(i, j));
+            double convection = - discretization_->computeDu2Dx(i,j) 
+                                - discretization_->computeDuvDy(i, j);
+
+            discretization_->f(i, j) = discretization_->u(i, j) 
+                                        + dt_ * (diffusion + convection + settings_.g[0]);
+        }
+    }
+
+    // Compute G
+
+    i_beg = discretization_->vIBegin();
+    i_end = discretization_->vIEnd();
+    j_beg = discretization_->vJBegin();
+    j_end = discretization_->vJEnd();
+
+    // Vertical
+    for(int j = j_beg + 1; j <= j_end - 1; j++){
+        discretization_->g(i_beg, j) = discretization_->v(i_beg, j);
+        discretization_->g(i_end, j) = discretization_->v(i_end, j);
+    }
+
+    // Horizontal
+    for(int i = i_beg + 1; i <= i_end - 1; i++){
+        discretization_->g(i, j_beg) = discretization_->v(i, j_beg);
+        discretization_->g(i, j_end) = discretization_->v(i, j_end);
+    }
+
+
+    // Interior
+    for(int i = i_beg + 1; i <= i_end - 1; i++){
+        for(int j = j_beg + 1 ; j <= j_end - 1; j++){
+
+            double diffusion = 1/settings_.re * (discretization_->computeD2vDx2(i, j) 
+                                               + discretization_->computeD2vDy2(i, j));
+            double convection = - discretization_->computeDv2Dy(i,j) 
+                                - discretization_->computeDuvDx(i, j);
+                                
+            discretization_->f(i, j) = discretization_->v(i, j) 
+                                        + dt_ * (diffusion + convection + settings_.g[1]);
+        }
+    }
+}
+
+
+void Computation::computeRightHandSide(){
+
+    int i_beg = discretization_->pIBegin();
+    int i_end = discretization_->pIEnd();
+    int j_beg = discretization_->pJBegin();
+    int j_end = discretization_->pJEnd();
+
+    // Interior
+    for(int i = i_beg + 1; i <= i_end - 1; i++){
+        for(int j = j_beg + 1 ; j <= j_end - 1; j++){
+            double dF = 1/discretization_-> dx() * (discretization_->f(i, j) - discretization_->f(i-1, j));
+            double dG = 1/discretization_-> dy() * (discretization_->g(i, j) - discretization_->g(i, j-1));
+            discretization_->rhs(i, j) = 1/dt_ * (dF + dG);
+        }
+    }
+
+
+
+}
 
 
     
