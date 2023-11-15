8aac6ed9b2b212f628a78ef75531496031292b19
Lukas Harsch <st186565@stud.uni-stuttgart.de>
2023-11-15 18:59:44 +0100

    [Documentation] changed documentation gauss_seidel

--- src/solver/gauss_seidel.h
+++ src/solver/gauss_seidel.h
@@ -4,16 +4,26 @@
 #include <iostream>
 
 /**
+ * @class GaussSeidel
  * @brief Standard Gauss-Seidel solver
  * 
  */
 class GaussSeidel : public PressureSolver {
 
 public:
-
+    /**
+     * @brief Constructor. 
+     * 
+     * @param data instance of Discretization holding the needed field variables for rhs and p
+     * @param epsilon tolerance for the solver 
+     * @param maximumNumberOfIterations maximum of iteration
+    */ 
     GaussSeidel(const std::shared_ptr<Discretization>& data, 
                         double epsilon, 
                         int maximumNumberOfIterations);
-
+    /**
+     * @brief override function that starts solver. 
+     * 
+    */ 
     void solve() override;
 };
